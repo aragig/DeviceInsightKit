2769218aef5c307f871f129964b9bade5a5e56b1
Toshihiko Arai <araemonz@gmail.com>
2024-10-22 09:15:48 +0900

    Initial Commit

---
+++ DeviceInsightKit/DeviceInsightKit.h
@@ -0,0 +1,18 @@
+//
+//  DeviceInsightKit.h
+//  DeviceInsightKit
+//
+//  Created by Toshihiko Arai on 2024/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeviceInsightKit.
+FOUNDATION_EXPORT double DeviceInsightKitVersionNumber;
+
+//! Project version string for DeviceInsightKit.
+FOUNDATION_EXPORT const unsigned char DeviceInsightKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceInsightKit/PublicHeader.h>
+
+
